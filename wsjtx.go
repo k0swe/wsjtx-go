68a1c4af34400de8697df25ed2a5e40088ff3597
Chris Keller <xylo04@gmail.com>
2020-10-30 19:45:58 -0600

    Add docs to WSJT-X message structs

--- wsjtx.go
+++ wsjtx.go
@@ -10,6 +10,18 @@ import (
 	"time"
 )
 
+/*
+The heartbeat  message shall be  sent on a periodic  basis every
+15   seconds.  This
+message is intended to be used by servers to detect the presence
+of a  client and also  the unexpected disappearance of  a client
+and  by clients  to learn  the schema  negotiated by  the server
+after it receives  the initial heartbeat message  from a client.
+
+Out/In.
+
+https://sourceforge.net/p/wsjt/wsjtx/ci/8f99fcce/tree/Network/NetworkMessage.hpp#l110
+*/
 type HeartbeatMessage struct {
 	Id        string `json:"id"`
 	MaxSchema uint32 `json:"maxSchemaVersion"`
@@ -17,6 +29,15 @@ type HeartbeatMessage struct {
 	Revision  string `json:"revision"`
 }
 
+/*
+WSJT-X  sends this  status message  when various  internal state
+changes to allow the server to  track the relevant state of each
+client without the need for  polling commands.
+
+Out only.
+
+https://sourceforge.net/p/wsjt/wsjtx/ci/8f99fcce/tree/Network/NetworkMessage.hpp#l141
+*/
 type StatusMessage struct {
 	Id                   string `json:"id"`
 	DialFrequency        uint64 `json:"dialFrequency"`
@@ -41,6 +62,17 @@ type StatusMessage struct {
 	ConfigurationName    string `json:"configName"`
 }
 
+/*
+The decode message is sent when  a new decode is completed, in
+this case the 'New' field is true. It is also used in response
+to  a "Replay"  message where  each  old decode  in the  "Band
+activity" window, that  has not been erased, is  sent in order
+as a one of these messages  with the 'New' field set to false.
+
+Out only.
+
+https://sourceforge.net/p/wsjt/wsjtx/ci/8f99fcce/tree/Network/NetworkMessage.hpp#l206
+*/
 type DecodeMessage struct {
 	Id               string  `json:"id"`
 	New              bool    `json:"new"`
@@ -54,10 +86,27 @@ type DecodeMessage struct {
 	OffAir           bool    `json:"offAir"`
 }
 
+/*
+This message is  send when all prior "Decode"  messages in the
+"Band Activity"  window have been discarded  and therefore are
+no long available for actioning  with a "Reply" message.
+
+Out/In.
+
+https://sourceforge.net/p/wsjt/wsjtx/ci/8f99fcce/tree/Network/NetworkMessage.hpp#l232
+*/
 type ClearMessage struct {
 	Id string `json:"id"`
 }
 
+/*
+The QSO logged message is sent when the WSJT-X user accepts the "Log  QSO" dialog by clicking
+the "OK" button.
+
+Out only.
+
+https://sourceforge.net/p/wsjt/wsjtx/ci/8f99fcce/tree/Network/NetworkMessage.hpp#l293
+*/
 type QsoLoggedMessage struct {
 	Id               string    `json:"id"`
 	DateTimeOff      time.Time `json:"dateTimeOff"`
@@ -78,10 +127,26 @@ type QsoLoggedMessage struct {
 	ExchangeReceived string    `json:"exchangeReceived"`
 }
 
+/*
+Close is  sent by  a client immediately  prior to  it shutting
+down gracefully.
+
+Out/In.
+
+https://sourceforge.net/p/wsjt/wsjtx/ci/8f99fcce/tree/Network/NetworkMessage.hpp#l318
+*/
 type CloseMessage struct {
 	Id string `json:"id"`
 }
 
+/*
+The decode message is sent when  a new decode is completed, in
+this case the 'New' field is true.
+
+Out only.
+
+https://sourceforge.net/p/wsjt/wsjtx/ci/8f99fcce/tree/Network/NetworkMessage.hpp#l381).
+*/
 type WSPRDecodeMessage struct {
 	Id        string  `json:"id"`
 	New       bool    `json:"new"`
@@ -96,6 +161,15 @@ type WSPRDecodeMessage struct {
 	OffAir    bool    `json:"offAir"`
 }
 
+/*
+The  logged ADIF  message is  sent to  the server(s)  when the
+WSJT-X user accepts the "Log  QSO" dialog by clicking the "OK"
+button.
+
+Out only.
+
+https://sourceforge.net/p/wsjt/wsjtx/ci/8f99fcce/tree/Network/NetworkMessage.hpp#l421
+*/
 type LoggedAdifMessage struct {
 	Id   string `json:"id"`
 	Adif string `json:"adif"`
