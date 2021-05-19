cbb8d0e78f4870291f606327a3885928f121751a
Chris Keller <xylo04@gmail.com>
2021-05-19 17:25:53 -0600

    Protect from buffer overruns
    If we're parsing messages from older versions of WSJT-X, there might be fewer fields than expected. That doesn't mean the entire parse result is bad, but now it's returned with some indication that it might not be complete.

--- parser.go
+++ parser.go
@@ -2,6 +2,7 @@ package wsjtx
 
 import (
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"github.com/leemcloughlin/jdn"
 	"math"
@@ -15,38 +16,59 @@ type parser struct {
 	cursor int
 }
 
+var ParseError = errors.New("parse error")
+var notEnoughBytes = fmt.Errorf("%w: fewer bytes than expected, maybe an older version of WSJTX", ParseError)
+
 // Parse messages following the interface laid out in
 // https://sourceforge.net/p/wsjt/wsjtx/ci/master/tree/Network/NetworkMessage.hpp. This only parses
 // "Out" or "In/Out" message types and does not include "In" types because they will never be
 // received by WSJT-X.
 func parseMessage(buffer []byte, length int) (interface{}, error) {
 	p := parser{buffer: buffer, length: length, cursor: 0}
-	m := p.parseUint32()
-	if m != magic {
-		return nil, fmt.Errorf("packet is not speaking the WSJT-X protocol")
+	m, err := p.parseUint32()
+	if err != nil {
+		return nil, ParseError
 	}
-	sch := p.parseUint32()
+	if m != magic {
+		return nil, fmt.Errorf("%w: packet is not speaking the WSJT-X protocol", ParseError)
+	}
+	sch, err := p.parseUint32()
+	if err != nil {
+		return nil, ParseError
+	}
 	if sch != schema {
-		return nil, fmt.Errorf("got a schema version I wasn't expecting: %d", sch)
+		return nil, fmt.Errorf("%w: got a schema version I wasn't expecting: %d", ParseError, sch)
 	}
 
-	messageType := p.parseUint32()
+	messageType, _ := p.parseUint32()
 	switch messageType {
 	case heartbeatNum:
-		heartbeat := p.parseHeartbeat()
-		err := p.checkParse(heartbeat)
+		heartbeat, err := p.parseHeartbeat()
+		if err != nil {
+			return heartbeat, err
+		}
+		err = p.checkParse(heartbeat)
 		return heartbeat, err
 	case statusNum:
-		status := p.parseStatus()
-		err := p.checkParse(status)
+		status, err := p.parseStatus()
+		if err != nil {
+			return status, err
+		}
+		err = p.checkParse(status)
 		return status, err
 	case decodeNum:
-		decode := p.parseDecode()
-		err := p.checkParse(decode)
+		decode, err := p.parseDecode()
+		if err != nil {
+			return decode, err
+		}
+		err = p.checkParse(decode)
 		return decode, err
 	case clearNum:
-		clear := p.parseClear()
-		err := p.checkParse(clear)
+		clear, err := p.parseClear()
+		if err != nil {
+			return clear, err
+		}
+		err = p.checkParse(clear)
 		return clear, err
 	case qsoLoggedNum:
 		qsoLogged, err := p.parseQsoLogged()
@@ -56,271 +78,240 @@ func parseMessage(buffer []byte, length int) (interface{}, error) {
 		err = p.checkParse(qsoLogged)
 		return qsoLogged, err
 	case closeNum:
-		closeMsg := p.parseClose()
-		err := p.checkParse(closeMsg)
+		closeMsg, err := p.parseClose()
+		if err != nil {
+			return closeMsg, err
+		}
+		err = p.checkParse(closeMsg)
 		return closeMsg, err
 	case wsprDecodeNum:
-		wspr := p.parseWsprDecode()
-		err := p.checkParse(wspr)
+		wspr, err := p.parseWsprDecode()
+		if err != nil {
+			return wspr, err
+		}
+		err = p.checkParse(wspr)
 		return wspr, err
 	case loggedAdifNum:
-		loggedAdif := p.parseLoggedAdif()
-		err := p.checkParse(loggedAdif)
+		loggedAdif, err := p.parseLoggedAdif()
+		if err != nil {
+			return loggedAdif, err
+		}
+		err = p.checkParse(loggedAdif)
 		return loggedAdif, err
 	}
-	return nil, nil
+	return nil, fmt.Errorf("%w: unknown message type %d", ParseError, messageType)
 }
 
 // Quick sanity check that we parsed all of the message bytes
 func (p *parser) checkParse(message interface{}) error {
 	if p.cursor != p.length {
-		return fmt.Errorf("parsing WSJT-X %s: there were %d bytes left over",
-			reflect.TypeOf(message).Name(), p.length-p.cursor)
+		return fmt.Errorf("%w %s: there were %d bytes left over",
+			ParseError, reflect.TypeOf(message).Name(), p.length-p.cursor)
 	}
 	return nil
 }
 
-func (p *parser) parseHeartbeat() HeartbeatMessage {
-	id := p.parseUtf8()
-	maxSchema := p.parseUint32()
-	version := p.parseUtf8()
-	revision := p.parseUtf8()
-	return HeartbeatMessage{
-		Id:        id,
-		MaxSchema: maxSchema,
-		Version:   version,
-		Revision:  revision,
-	}
+func (p *parser) parseHeartbeat() (HeartbeatMessage, error) {
+	var err error
+	heartbeatMessage := HeartbeatMessage{}
+	heartbeatMessage.Id, err = p.parseUtf8()
+	heartbeatMessage.MaxSchema, err = p.parseUint32()
+	heartbeatMessage.Version, err = p.parseUtf8()
+	heartbeatMessage.Revision, err = p.parseUtf8()
+	return heartbeatMessage, err
 }
 
-func (p *parser) parseStatus() StatusMessage {
-	id := p.parseUtf8()
-	dialFreq := p.parseUint64()
-	mode := p.parseUtf8()
-	dxCall := p.parseUtf8()
-	report := p.parseUtf8()
-	txMode := p.parseUtf8()
-	txEnabled := p.parseBool()
-	transmitting := p.parseBool()
-	decoding := p.parseBool()
-	rxDf := p.parseUint32()
-	txDf := p.parseUint32()
-	deCall := p.parseUtf8()
-	deGrid := p.parseUtf8()
-	dxGrid := p.parseUtf8()
-	txWatchdog := p.parseBool()
-	subMode := p.parseUtf8()
-	fastMode := p.parseBool()
-	specialMode := p.parseUint8()
-	freqTolerance := p.parseUint32()
-	trPeriod := p.parseUint32()
-	configName := p.parseUtf8()
-	txMessage := p.parseUtf8()
-	return StatusMessage{
-		Id:                   id,
-		DialFrequency:        dialFreq,
-		Mode:                 mode,
-		DxCall:               dxCall,
-		Report:               report,
-		TxMode:               txMode,
-		TxEnabled:            txEnabled,
-		Transmitting:         transmitting,
-		Decoding:             decoding,
-		RxDF:                 rxDf,
-		TxDF:                 txDf,
-		DeCall:               deCall,
-		DeGrid:               deGrid,
-		DxGrid:               dxGrid,
-		TxWatchdog:           txWatchdog,
-		SubMode:              subMode,
-		FastMode:             fastMode,
-		SpecialOperationMode: specialMode,
-		FrequencyTolerance:   freqTolerance,
-		TRPeriod:             trPeriod,
-		ConfigurationName:    configName,
-		TxMessage:            txMessage,
-	}
+func (p *parser) parseStatus() (StatusMessage, error) {
+	var err error
+	statusMessage := StatusMessage{}
+	statusMessage.Id, err = p.parseUtf8()
+	statusMessage.DialFrequency, err = p.parseUint64()
+	statusMessage.Mode, err = p.parseUtf8()
+	statusMessage.DxCall, err = p.parseUtf8()
+	statusMessage.Report, err = p.parseUtf8()
+	statusMessage.TxMode, err = p.parseUtf8()
+	statusMessage.TxEnabled, err = p.parseBool()
+	statusMessage.Transmitting, err = p.parseBool()
+	statusMessage.Decoding, err = p.parseBool()
+	statusMessage.RxDF, err = p.parseUint32()
+	statusMessage.TxDF, err = p.parseUint32()
+	statusMessage.DeCall, err = p.parseUtf8()
+	statusMessage.DeGrid, err = p.parseUtf8()
+	statusMessage.DxGrid, err = p.parseUtf8()
+	statusMessage.TxWatchdog, err = p.parseBool()
+	statusMessage.SubMode, err = p.parseUtf8()
+	statusMessage.FastMode, err = p.parseBool()
+	statusMessage.SpecialOperationMode, err = p.parseUint8()
+	statusMessage.FrequencyTolerance, err = p.parseUint32()
+	statusMessage.TRPeriod, err = p.parseUint32()
+	statusMessage.ConfigurationName, err = p.parseUtf8()
+	statusMessage.TxMessage, err = p.parseUtf8()
+	return statusMessage, err
 }
 
-func (p *parser) parseDecode() DecodeMessage {
-	id := p.parseUtf8()
-	newDecode := p.parseBool()
-	decodeTime := p.parseUint32()
-	snr := p.parseInt32()
-	deltaTime := p.parseFloat64()
-	deltaFreq := p.parseUint32()
-	mode := p.parseUtf8()
-	message := p.parseUtf8()
-	lowConfidence := p.parseBool()
-	offAir := p.parseBool()
-	return DecodeMessage{
-		Id:               id,
-		New:              newDecode,
-		Time:             decodeTime,
-		Snr:              snr,
-		DeltaTimeSec:     deltaTime,
-		DeltaFrequencyHz: deltaFreq,
-		Mode:             mode,
-		Message:          message,
-		LowConfidence:    lowConfidence,
-		OffAir:           offAir,
-	}
+func (p *parser) parseDecode() (DecodeMessage, error) {
+	var err error
+	decodeMessage := DecodeMessage{}
+	decodeMessage.Id, err = p.parseUtf8()
+	decodeMessage.New, err = p.parseBool()
+	decodeMessage.Time, err = p.parseUint32()
+	decodeMessage.Snr, err = p.parseInt32()
+	decodeMessage.DeltaTimeSec, err = p.parseFloat64()
+	decodeMessage.DeltaFrequencyHz, err = p.parseUint32()
+	decodeMessage.Mode, err = p.parseUtf8()
+	decodeMessage.Message, err = p.parseUtf8()
+	decodeMessage.LowConfidence, err = p.parseBool()
+	decodeMessage.OffAir, err = p.parseBool()
+	return decodeMessage, err
 }
 
-func (p *parser) parseClear() ClearMessage {
-	id := p.parseUtf8()
-	return ClearMessage{
-		Id: id,
-	}
+func (p *parser) parseClear() (ClearMessage, error) {
+	var err error
+	clearMessage := ClearMessage{}
+	clearMessage.Id, err = p.parseUtf8()
+	return clearMessage, err
 }
 
 func (p *parser) parseQsoLogged() (QsoLoggedMessage, error) {
-	var empty QsoLoggedMessage
-	id := p.parseUtf8()
-	timeOff, err := p.parseQDateTime()
-	if err != nil {
-		return empty, err
-	}
-	dxCall := p.parseUtf8()
-	dxGrid := p.parseUtf8()
-	txFrequency := p.parseUint64()
-	mode := p.parseUtf8()
-	reportSent := p.parseUtf8()
-	reportReceived := p.parseUtf8()
-	txPower := p.parseUtf8()
-	comments := p.parseUtf8()
-	name := p.parseUtf8()
-	timeOn, err := p.parseQDateTime()
-	if err != nil {
-		return empty, err
-	}
-	operatorCall := p.parseUtf8()
-	myCall := p.parseUtf8()
-	myGrid := p.parseUtf8()
-	exchangeSent := p.parseUtf8()
-	exchangeReceived := p.parseUtf8()
-	return QsoLoggedMessage{
-		Id:               id,
-		DateTimeOff:      timeOff,
-		DxCall:           dxCall,
-		DxGrid:           dxGrid,
-		TxFrequency:      txFrequency,
-		Mode:             mode,
-		ReportSent:       reportSent,
-		ReportReceived:   reportReceived,
-		TxPower:          txPower,
-		Comments:         comments,
-		Name:             name,
-		DateTimeOn:       timeOn,
-		OperatorCall:     operatorCall,
-		MyCall:           myCall,
-		MyGrid:           myGrid,
-		ExchangeSent:     exchangeSent,
-		ExchangeReceived: exchangeReceived,
-	}, nil
+	var err error
+	qsoLoggedMessage := QsoLoggedMessage{}
+	qsoLoggedMessage.Id, err = p.parseUtf8()
+	qsoLoggedMessage.DateTimeOff, err = p.parseQDateTime()
+	qsoLoggedMessage.DxCall, err = p.parseUtf8()
+	qsoLoggedMessage.DxGrid, err = p.parseUtf8()
+	qsoLoggedMessage.TxFrequency, err = p.parseUint64()
+	qsoLoggedMessage.Mode, err = p.parseUtf8()
+	qsoLoggedMessage.ReportSent, err = p.parseUtf8()
+	qsoLoggedMessage.ReportReceived, err = p.parseUtf8()
+	qsoLoggedMessage.TxPower, err = p.parseUtf8()
+	qsoLoggedMessage.Comments, err = p.parseUtf8()
+	qsoLoggedMessage.Name, err = p.parseUtf8()
+	qsoLoggedMessage.DateTimeOn, err = p.parseQDateTime()
+	qsoLoggedMessage.OperatorCall, err = p.parseUtf8()
+	qsoLoggedMessage.MyCall, err = p.parseUtf8()
+	qsoLoggedMessage.MyGrid, err = p.parseUtf8()
+	qsoLoggedMessage.ExchangeSent, err = p.parseUtf8()
+	qsoLoggedMessage.ExchangeReceived, err = p.parseUtf8()
+	return qsoLoggedMessage, err
 }
 
-func (p *parser) parseClose() interface{} {
-	id := p.parseUtf8()
-	return CloseMessage{
-		Id: id,
-	}
+func (p *parser) parseClose() (CloseMessage, error) {
+	var err error
+	closeMessage := CloseMessage{}
+	closeMessage.Id, err = p.parseUtf8()
+	return closeMessage, err
 }
 
-func (p *parser) parseWsprDecode() interface{} {
-	id := p.parseUtf8()
-	newDecode := p.parseBool()
-	decodeTime := p.parseUint32()
-	snr := p.parseInt32()
-	deltaTime := p.parseFloat64()
-	frequency := p.parseUint64()
-	drift := p.parseInt32()
-	callsign := p.parseUtf8()
-	grid := p.parseUtf8()
-	power := p.parseInt32()
-	offAir := p.parseBool()
-	return WSPRDecodeMessage{
-		Id:        id,
-		New:       newDecode,
-		Time:      decodeTime,
-		Snr:       snr,
-		DeltaTime: deltaTime,
-		Frequency: frequency,
-		Drift:     drift,
-		Callsign:  callsign,
-		Grid:      grid,
-		Power:     power,
-		OffAir:    offAir,
-	}
+func (p *parser) parseWsprDecode() (WSPRDecodeMessage, error) {
+	var err error
+	wsprDecodeMessage := WSPRDecodeMessage{}
+	wsprDecodeMessage.Id, err = p.parseUtf8()
+	wsprDecodeMessage.New, err = p.parseBool()
+	wsprDecodeMessage.Time, err = p.parseUint32()
+	wsprDecodeMessage.Snr, err = p.parseInt32()
+	wsprDecodeMessage.DeltaTime, err = p.parseFloat64()
+	wsprDecodeMessage.Frequency, err = p.parseUint64()
+	wsprDecodeMessage.Drift, err = p.parseInt32()
+	wsprDecodeMessage.Callsign, err = p.parseUtf8()
+	wsprDecodeMessage.Grid, err = p.parseUtf8()
+	wsprDecodeMessage.Power, err = p.parseInt32()
+	wsprDecodeMessage.OffAir, err = p.parseBool()
+	return wsprDecodeMessage, err
 }
 
-func (p *parser) parseLoggedAdif() interface{} {
-	id := p.parseUtf8()
-	adif := p.parseUtf8()
-	return LoggedAdifMessage{
-		Id:   id,
-		Adif: adif,
-	}
+func (p *parser) parseLoggedAdif() (LoggedAdifMessage, error) {
+	var err error
+	loggedAdifMessage := LoggedAdifMessage{}
+	loggedAdifMessage.Id, err = p.parseUtf8()
+	loggedAdifMessage.Adif, err = p.parseUtf8()
+	return loggedAdifMessage, err
 }
 
-func (p *parser) parseUint8() uint8 {
+func (p *parser) parseUint8() (uint8, error) {
+	if len(p.buffer) < p.cursor {
+		return 0, notEnoughBytes
+	}
 	value := p.buffer[p.cursor]
 	p.cursor += 1
-	return value
+	return value, nil
 }
 
-func (p *parser) parseUtf8() string {
-	strlen := p.parseUint32()
+func (p *parser) parseUtf8() (string, error) {
+	strlen, err := p.parseUint32()
+	if err != nil {
+		return "", err
+	}
 	if strlen == uint32(qDataStreamNull) {
 		// this is a sentinel value meaning "null" in QDataStream, but Golang can't have nil strings
 		strlen = 0
 	}
-	value := string(p.buffer[p.cursor:(p.cursor + int(strlen))])
+	end := p.cursor + int(strlen)
+	value := string(p.buffer[p.cursor:end])
 	p.cursor += int(strlen)
-	return value
+	return value, nil
 }
 
-func (p *parser) parseUint32() uint32 {
-	value := binary.BigEndian.Uint32(p.buffer[p.cursor : p.cursor+4])
+func (p *parser) parseUint32() (uint32, error) {
+	end := p.cursor + 4
+	if len(p.buffer) < end {
+		return 0, notEnoughBytes
+	}
+	value := binary.BigEndian.Uint32(p.buffer[p.cursor:end])
 	p.cursor += 4
-	return value
+	return value, nil
 }
 
-func (p *parser) parseInt32() int32 {
-	value := int32(binary.BigEndian.Uint32(p.buffer[p.cursor : p.cursor+4]))
+func (p *parser) parseInt32() (int32, error) {
+	end := p.cursor + 4
+	if len(p.buffer) < end {
+		return 0, notEnoughBytes
+	}
+	value := int32(binary.BigEndian.Uint32(p.buffer[p.cursor:end]))
 	p.cursor += 4
-	return value
+	return value, nil
 }
 
-func (p *parser) parseUint64() uint64 {
-	value := binary.BigEndian.Uint64(p.buffer[p.cursor : p.cursor+8])
+func (p *parser) parseUint64() (uint64, error) {
+	end := p.cursor + 8
+	if len(p.buffer) < end {
+		return 0, notEnoughBytes
+	}
+	value := binary.BigEndian.Uint64(p.buffer[p.cursor:end])
 	p.cursor += 8
-	return value
+	return value, nil
 }
 
-func (p *parser) parseFloat64() float64 {
-	bits := binary.BigEndian.Uint64(p.buffer[p.cursor : p.cursor+8])
+func (p *parser) parseFloat64() (float64, error) {
+	end := p.cursor + 8
+	if len(p.buffer) < end {
+		return 0, notEnoughBytes
+	}
+	bits := binary.BigEndian.Uint64(p.buffer[p.cursor:end])
 	value := math.Float64frombits(bits)
 	p.cursor += 8
-	return value
+	return value, nil
 }
 
-func (p *parser) parseBool() bool {
+func (p *parser) parseBool() (bool, error) {
+	if len(p.buffer) < p.cursor {
+		return false, notEnoughBytes
+	}
 	value := p.buffer[p.cursor] != 0
 	p.cursor += 1
-	return value
+	return value, nil
 }
 
 func (p *parser) parseQDateTime() (time.Time, error) {
-	julianDay := p.parseUint64()
+	julianDay, err := p.parseUint64()
 	year, month, day := jdn.FromNumber(int(julianDay))
-	msSinceMidnight := int(p.parseUint32())
+	msMid, err := p.parseUint32()
+	msSinceMidnight := int(msMid)
 	hour := msSinceMidnight / 3600000
 	msSinceMidnight -= hour * 3600000
 	minute := msSinceMidnight / 60000
 	msSinceMidnight -= minute * 60000
 	second := msSinceMidnight / 1000
-	timespec := p.parseUint8()
+	timespec, err := p.parseUint8()
 	var value time.Time
 	switch timespec {
 	case 0:
@@ -332,5 +323,5 @@ func (p *parser) parseQDateTime() (time.Time, error) {
 	default:
 		return value, fmt.Errorf("got a timespec I wasn't expecting: %d", timespec)
 	}
-	return value, nil
+	return value, err
 }
--- parser_test.go
+++ parser_test.go
@@ -2,6 +2,7 @@ package wsjtx
 
 import (
 	"encoding/hex"
+	"errors"
 	"reflect"
 	"testing"
 	"time"
@@ -12,54 +13,59 @@ type parseArgs struct {
 	length int
 }
 
+type parseResult struct {
+	message interface{}
+	err     error
+}
+
 func TestParseMessage(t *testing.T) {
 
 	tests := []struct {
 		name string
 		args parseArgs
-		want interface{}
+		want parseResult
 	}{
 		{
 			name: "Parse Heartbeat",
 			args: argsFrom(`adbccbda00000002000000000000000657534a542d580000000300000005322e322e3200000006306439623936`),
-			want: HeartbeatMessage{
+			want: parseResult{HeartbeatMessage{
 				Id:        "WSJT-X",
 				MaxSchema: 3,
 				Version:   "2.2.2",
 				Revision:  "0d9b96",
-			},
+			}, nil},
 		},
-		//{
-		//	name: "Parse Status 2.2.2",
-		//	args: argsFrom(`adbccbda00000002000000010000000657534a542d5800000000006bf0d000000003465438ffffffff000000032d313500000003465438000000000003730000079e000000054b3053574500000006444d37394c56ffffffff00ffffffff0000ffffffffffffffff0000000744656661756c74`),
-		//	want: StatusMessage{
-		//		Id:                   "WSJT-X",
-		//		DialFrequency:        7074000,
-		//		Mode:                 "FT8",
-		//		DxCall:               "",
-		//		Report:               "-15",
-		//		TxMode:               "FT8",
-		//		TxEnabled:            false,
-		//		Transmitting:         false,
-		//		Decoding:             false,
-		//		RxDF:                 883,
-		//		TxDF:                 1950,
-		//		DeCall:               "K0SWE",
-		//		DeGrid:               "DM79LV",
-		//		DxGrid:               "",
-		//		TxWatchdog:           false,
-		//		SubMode:              "",
-		//		FastMode:             false,
-		//		SpecialOperationMode: 0,
-		//		FrequencyTolerance:   4294967295,
-		//		TRPeriod:             4294967295,
-		//		ConfigurationName:    "Default",
-		//	},
-		//},
+		{
+			name: "Parse Status 2.2.2",
+			args: argsFrom(`adbccbda00000002000000010000000657534a542d5800000000006bf0d000000003465438ffffffff000000032d313500000003465438000000000003730000079e000000054b3053574500000006444d37394c56ffffffff00ffffffff0000ffffffffffffffff0000000744656661756c74`),
+			want: parseResult{StatusMessage{
+				Id:                   "WSJT-X",
+				DialFrequency:        7074000,
+				Mode:                 "FT8",
+				DxCall:               "",
+				Report:               "-15",
+				TxMode:               "FT8",
+				TxEnabled:            false,
+				Transmitting:         false,
+				Decoding:             false,
+				RxDF:                 883,
+				TxDF:                 1950,
+				DeCall:               "K0SWE",
+				DeGrid:               "DM79LV",
+				DxGrid:               "",
+				TxWatchdog:           false,
+				SubMode:              "",
+				FastMode:             false,
+				SpecialOperationMode: 0,
+				FrequencyTolerance:   4294967295,
+				TRPeriod:             4294967295,
+				ConfigurationName:    "Default",
+			}, ParseError},
+		},
 		{
 			name: "Parse Status 2.3.1",
 			args: argsFrom(`adbccbda00000002000000010000000657534a542d5800000000006bf0d000000003465438ffffffff000000032d313500000003465438000000000003730000079e000000054b3053574500000006444d37394c56ffffffff00ffffffff0000ffffffffffffffff0000000744656661756c7400000000`),
-			want: StatusMessage{
+			want: parseResult{StatusMessage{
 				Id:                   "WSJT-X",
 				DialFrequency:        7074000,
 				Mode:                 "FT8",
@@ -82,12 +88,12 @@ func TestParseMessage(t *testing.T) {
 				TRPeriod:             4294967295,
 				ConfigurationName:    "Default",
 				TxMessage:            "",
-			},
+			}, nil},
 		},
 		{
 			name: "Parse Decode",
 			args: argsFrom(`adbccbda00000002000000020000000657534a542d58010259baf8fffffffb3fc99999a000000000000516000000017e0000000e4a4132454a50204e3442502037330000`),
-			want: DecodeMessage{
+			want: parseResult{DecodeMessage{
 				Id:               "WSJT-X",
 				New:              true,
 				Time:             39435000,
@@ -98,19 +104,19 @@ func TestParseMessage(t *testing.T) {
 				Message:          "JA2EJP N4BP 73",
 				LowConfidence:    false,
 				OffAir:           false,
-			},
+			}, nil},
 		},
 		{
 			name: "Parse Clear",
 			args: argsFrom(`adbccbda00000002000000030000000657534a542d58`),
-			want: ClearMessage{
+			want: parseResult{ClearMessage{
 				Id: "WSJT-X",
-			},
+			}, nil},
 		},
 		{
 			name: "Parse QSO Logged",
 			args: argsFrom(`adbccbda00000002000000050000000657534a542d5800000000002586110277ac48010000000454335354000000044a4b373300000000006bf86e00000003465438000000022d33000000022d37000000013500000007436f6d6d656e74000000034a6f6500000000002586110276c1e801000000055433535452000000054b3053574500000006444d37394c56000000023142000000023144`),
-			want: QsoLoggedMessage{
+			want: parseResult{QsoLoggedMessage{
 				Id:               "WSJT-X",
 				DateTimeOff:      parseTime("2020-10-30 11:29:57 +0000 UTC"),
 				DxCall:           "T3ST",
@@ -128,19 +134,19 @@ func TestParseMessage(t *testing.T) {
 				MyGrid:           "DM79LV",
 				ExchangeSent:     "1B",
 				ExchangeReceived: "1D",
-			},
+			}, nil},
 		},
 		{
 			name: "Parse Close",
 			args: argsFrom(`adbccbda00000002000000060000000657534a542d58`),
-			want: CloseMessage{
+			want: parseResult{CloseMessage{
 				Id: "WSJT-X",
-			},
+			}, nil},
 		},
 		{
 			name: "Parse WSPR Decode",
 			args: argsFrom(`adbccbda000000020000000a0000000657534a542d580102b5f840ffffffeebfe000000000000000000000006b6c7300000000000000054b3654475700000004434d39350000001700`),
-			want: WSPRDecodeMessage{
+			want: parseResult{WSPRDecodeMessage{
 				Id:        "WSJT-X",
 				New:       true,
 				Time:      45480000,
@@ -152,28 +158,28 @@ func TestParseMessage(t *testing.T) {
 				Grid:      "CM95",
 				Power:     23,
 				OffAir:    false,
-			},
+			}, nil},
 		},
 		{
 			name: "Parse Logged Adif",
 			args: argsFrom(`adbccbda000000020000000c0000000657534a542d580000015c0a3c616469665f7665723a353e332e312e300a3c70726f6772616d69643a363e57534a542d580a3c454f483e0a3c63616c6c3a343e54335354203c677269647371756172653a343e4a4b3733203c6d6f64653a333e465438203c7273745f73656e743a323e2d38203c7273745f726376643a323e2d39203c71736f5f646174653a383e3230323031303330203c74696d655f6f6e3a363e313230383136203c71736f5f646174655f6f66663a383e3230323031303330203c74696d655f6f66663a363e313230393136203c62616e643a333e34306d203c667265713a383e372e303735393530203c73746174696f6e5f63616c6c7369676e3a353e4b30535745203c6d795f677269647371756172653a363e444d37394c56203c74785f7077723a313e35203c636f6d6d656e743a373e436f6d6d656e74203c6e616d653a343e4a657373203c6f70657261746f723a353e5433535452203c454f523e`),
-			want: LoggedAdifMessage{
+			want: parseResult{LoggedAdifMessage{
 				Id: "WSJT-X",
 				Adif: `
 <adif_ver:5>3.1.0
 <programid:6>WSJT-X
 <EOH>
 <call:4>T3ST <gridsquare:4>JK73 <mode:3>FT8 <rst_sent:2>-8 <rst_rcvd:2>-9 <qso_date:8>20201030 <time_on:6>120816 <qso_date_off:8>20201030 <time_off:6>120916 <band:3>40m <freq:8>7.075950 <station_callsign:5>K0SWE <my_gridsquare:6>DM79LV <tx_pwr:1>5 <comment:7>Comment <name:4>Jess <operator:5>T3STR <EOR>`,
-			},
+			}, nil},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := parseMessage(tt.args.buffer, tt.args.length)
-			if err != nil {
+			if !errors.Is(err, tt.want.err) {
 				t.Error(err)
 			}
-			if !reflect.DeepEqual(got, tt.want) {
+			if !reflect.DeepEqual(got, tt.want.message) {
 				t.Errorf("parseMessage() = %v, want %v", got, tt.want)
 			}
 		})
