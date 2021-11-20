6d745c4c028087685d08f5bbc63ab2f8c22c716e
Chris Keller <xylo04@gmail.com>
2021-11-19 18:21:59 -0700

    Add integration tests for all send methods

--- integration/send_test.go
+++ integration/send_test.go
@@ -1,6 +1,8 @@
 package integration
 
 import (
+	"image/color"
+	"math"
 	"time"
 
 	"github.com/k0swe/wsjtx-go/v3"
@@ -18,6 +20,158 @@ func (s *integrationTestSuite) TestSendHeartbeat() {
 
 	s.T().Log("test case sending heartbeat struct")
 	err := s.server.Heartbeat(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendClear() {
+	s.primeConnection()
+
+	msg := wsjtx.ClearMessage{Id: "WSJT-X", Window: 2}
+	want := decode(`adbccbda00000002000000030000000657534a542d5802`)
+
+	s.T().Log("test case sending clear struct")
+	err := s.server.Clear(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendReply() {
+	s.primeConnection()
+
+	msg := wsjtx.ReplyMessage{
+		Id:               "WSJT-X",
+		Time:             1234,
+		Snr:              -15,
+		DeltaTimeSec:     0.5,
+		DeltaFrequencyHz: 2345,
+		Mode:             "FT8",
+		Message:          "CQ K0SWE DM79",
+		LowConfidence:    false,
+		Modifiers:        0,
+	}
+	want := decode(`adbccbda00000002000000040000000657534a542d58000004d2fffffff13fe000000000000000000929000000034654380000000d4351204b3053574520444d37390000`)
+
+	s.T().Log("test case sending reply struct")
+	err := s.server.Reply(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendClose() {
+	s.primeConnection()
+
+	msg := wsjtx.CloseMessage{Id: "WSJT-X"}
+	want := decode(`adbccbda00000002000000060000000657534a542d58`)
+
+	s.T().Log("test case sending close struct")
+	err := s.server.Close(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendReplay() {
+	s.primeConnection()
+
+	msg := wsjtx.ReplayMessage{Id: "WSJT-X"}
+	want := decode(`adbccbda00000002000000070000000657534a542d58`)
+
+	s.T().Log("test case sending replay struct")
+	err := s.server.Replay(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendHaltTx() {
+	s.primeConnection()
+
+	msg := wsjtx.HaltTxMessage{Id: "WSJT-X", AutoTxOnly: false}
+	want := decode(`adbccbda00000002000000080000000657534a542d5800`)
+
+	s.T().Log("test case sending haltTx struct")
+	err := s.server.HaltTx(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendFreeText() {
+	s.primeConnection()
+
+	msg := wsjtx.FreeTextMessage{
+		Id:   "WSJT-X",
+		Text: "J72IMS K0SWE R-15",
+		Send: true,
+	}
+	want := decode(`adbccbda00000002000000090000000657534a542d58000000114a3732494d53204b3053574520522d313501`)
+
+	s.T().Log("test case sending freeText struct")
+	err := s.server.FreeText(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendLocation() {
+	s.primeConnection()
+
+	msg := wsjtx.LocationMessage{Id: "WSJT-X", Location: "DM79jx"}
+	want := decode(`adbccbda000000020000000b0000000657534a542d5800000006444d37396a78`)
+
+	s.T().Log("test case sending location struct")
+	err := s.server.Location(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendHighlightCallsign() {
+	s.primeConnection()
+
+	msg := wsjtx.HighlightCallsignMessage{
+		Id:              "WSJT-X",
+		Callsign:        "KM4ACK",
+		BackgroundColor: color.RGBA{R: 255},
+		ForegroundColor: color.Black,
+		HighlightLast:   true,
+		Reset:           false,
+	}
+	want := decode(`adbccbda000000020000000d0000000657534a542d58000000064b4d3441434b010000ffff00000000000001ffff000000000000000001`)
+
+	s.T().Log("test case sending highlightCallsign struct")
+	err := s.server.HighlightCallsign(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendSwitchConfiguration() {
+	s.primeConnection()
+
+	msg := wsjtx.SwitchConfigurationMessage{Id: "WSJT-X", ConfigurationName: "IC-7300"}
+	want := decode(`adbccbda000000020000000e0000000657534a542d580000000749432d37333030`)
+
+	s.T().Log("test case sending switchConfiguration struct")
+	err := s.server.SwitchConfiguration(msg)
+	s.Require().NoError(err)
+	s.waitForReceiveAndCheck(want)
+}
+
+func (s *integrationTestSuite) TestSendConfigure() {
+	s.primeConnection()
+
+	msg := wsjtx.ConfigureMessage{
+		Id:                 "WSJT-X",
+		Mode:               "JT9",
+		FrequencyTolerance: math.MaxInt32,
+		Submode:            "",
+		FastMode:           false,
+		TRPeriod:           math.MaxInt32,
+		RxDF:               1500,
+		DXCall:             "KI6NAZ",
+		DXGrid:             "DM03",
+		GenerateMessages:   false,
+	}
+	want := decode(`adbccbda000000020000000f0000000657534a542d58000000034a54397fffffffffffffff007fffffff000005dc000000064b49364e415a00000004444d303300`)
+
+	s.T().Log("test case sending configure struct")
+	err := s.server.Configure(msg)
 	s.Require().NoError(err)
 	s.waitForReceiveAndCheck(want)
 }
