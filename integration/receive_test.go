c8211c1fde353eb51e9874173aca8f1fdbfb2e35
Chris Keller <xylo04@gmail.com>
2026-01-17 22:52:08 -0700

    Use Local timezone
    JTDX sends its date times over the wire with [`QDateTime::timeSpec`](https://doc.qt.io/qt-6/qdatetime.html#timeSpec) of 0 for local, unzoned date-times. Rather than trying to make any assumptions, this binding will pass that through.

--- integration/receive_test.go
+++ integration/receive_test.go
@@ -175,7 +175,7 @@ func (s *integrationTestSuite) TestReceiveCases() {
 			want: receiveResult{
 				msg: wsjtx.QsoLoggedMessage{
 					Id:             "JTDX",
-					DateTimeOff:    parseTime("2026-01-05 04:31:00 +0000 UTC"),
+					DateTimeOff:    time.Date(2026, time.January, 5, 12, 31, 0, 0, time.Local),
 					DxCall:         "T3ST",
 					DxGrid:         "JK73",
 					TxFrequency:    14075500,
@@ -185,7 +185,7 @@ func (s *integrationTestSuite) TestReceiveCases() {
 					TxPower:        "20",
 					Comments:       "Distance: 10773 km",
 					Name:           "Smith",
-					DateTimeOn:     parseTime("2026-01-05 04:29:47 +0000 UTC"),
+					DateTimeOn:     time.Date(2026, time.January, 5, 12, 29, 47, 0, time.Local),
 					OperatorCall:   "BG5VLI",
 					MyCall:         "BG5VLI",
 					MyGrid:         "OL94",
